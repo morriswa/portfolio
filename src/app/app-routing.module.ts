7be3f64b288aab3786da3b7f1dec5a86f6c4415d
morriswa <william@morriswa.org>
2023-04-01 10:39:35 -0500

    create education component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EducationComponent } from './components/education/education.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'education', component: EducationComponent },
+  { path: '', redirectTo: '/education', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
+// NG
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// APP
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EducationComponent } from './components/education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
